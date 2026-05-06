1c2bf2806878996e40e8197c8ab37bb608d9b5cd
jeptiahservices-hash <jeptiah.services@gmail.com>
2026-05-06 22:02:09 +0800

    Update data.js

--- sys_mycar/data.js
+++ sys_mycar/data.js
@@ -1,7 +1,7 @@
 const serviceRecords = {
     "JXB2663": {
         customerName: "Jaefri Fathiah",
-        carImage: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?q=80&w=600",
+        carImage: "img_car/mycar_jxb2663.jpg",
         lastService: "2026-04-15",
         mileage: "85,000 km",
         partsChanged: ["Oil Filter (Bosch)", "Fully Synthetic Oil 5W-40", "Drain Plug Washer"],
@@ -14,6 +14,17 @@ const serviceRecords = {
         customerName: "Fathiah",
         carImage: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?q=80&w=600",
         lastService: "2026-05-01",
+        mileage: "120,000 km",
+        partsChanged: ["Brake Pads (Front)", "Brake Fluid Flush"],
+        details: "Customer reported squeaking noise. Replaced worn pads.",
+        driveLink: "https://drive.google.com/file/d/ANOTHER_ID/view",
+        nextService: "2026-11-01"
+    },
+
+    "ABC1234": {
+        customerName: "tiah",
+        carImage: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?q=80&w=600",
+        lastService: "20265-05-01",
         mileage: "120,000 km",
         partsChanged: ["Brake Pads (Front)", "Brake Fluid Flush"],
         details: "Customer reported squeaking noise. Replaced worn pads.",
