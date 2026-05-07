ce03f5ff8c7e96e336130902bb5ae851cf452412
jeptiahservices-hash <jeptiah.services@gmail.com>
2026-05-07 14:16:40 +0800

    Update data.js

--- sys_mycar/data.js
+++ sys_mycar/data.js
@@ -23,15 +23,15 @@ const serviceRecords = {
         nextService: "2026-11-01"
     },
 
-    "ABC1223": {
-        customerName: "tiah",
-        cardetails: "Proton Wira (Red)",
-        carImage: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?q=80&w=600",
-        lastService: "20265-05-01",
-        mileage: "120,000 km",
-        partsChanged: ["Brake Pads (Front)", "Brake Fluid Flush"],
-        details: "Customer reported squeaking noise. Replaced worn pads.",
-        driveLink: "https://drive.google.com/file/d/ANOTHER_ID/view",
-        nextService: "2026-11-01"
+    "WGF5135": {
+        customerName: "Kak Miera",
+        cardetails: "Proton Wira (White)",
+        carImage: "img_car/mycar_wgf5135.jpeg",
+        lastService: "2026-05-007",
+        mileage: "000,000 km",
+        partsChanged: ["Replace Radiator", "Add Coolant", "Repair Signal Flasher"],
+        details: "Customer Request Replacing Radiator. Include Radiator Coolant.",
+        driveLink: "https://drive.google.com/file/d/1kSdyNS4xTaILozT6e0G8CpFHBWW4reKk/view?usp=sharing",
+        nextService: "2026-00-00"
     }
 };
